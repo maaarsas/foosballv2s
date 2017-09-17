f8ab08f918cf9ef6e3d9729b20edae3ca55f1c80
Martynas Šapalas <martynas.sapalas@mif.stud.vu.lt>
2017-09-17 12:16:04 +0300

    Add classes for videos

---
+++ foosballv2s/VideoLive.cs
@@ -0,0 +1,19 @@
+﻿using Emgu.CV;
+using System;
+
+namespace foosballv2s
+{
+    class VideoLive : IVideo
+    {
+        // TODO: Implement the class for getting frames from live stream
+        public Mat GetFrame()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+
+        }
+    }
+}

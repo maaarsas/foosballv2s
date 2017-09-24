ef53a862e34cc5c4a916082f50405b06abd3dbc5
DanielRitvas <uzastish@gmail.com>
2017-09-24 20:05:03 +0300

    initial branch commit. Add video streaming on screen

---
+++ foosballXamarin/MainActivity.cs
@@ -0,0 +1,51 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Hardware;
+using Android.Views;
+using System;
+
+namespace foosballXamarin
+{
+    [Activity(Label = "foosballXamarin", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        bool _previewing;
+        Camera _camera;
+        TextureView _textureView;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            SetContentView(Resource.Layout.Main);
+
+            Button button = FindViewById<Button>(Resource.Id.button1);
+            _textureView = FindViewById<TextureView>(Resource.Id.textureView1);
+            button.Click += delegate
+            {
+                try
+                {
+                    if (!_previewing)
+                    {
+                        _camera = Camera.Open();
+                        _camera.SetPreviewTexture(_textureView.SurfaceTexture);
+                        _camera.StartPreview();
+                    }
+                    else
+                    {
+                        _camera.StopPreview();
+                        _camera.Release();
+                    }
+                }
+                catch (Java.IO.IOException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    _previewing = !_previewing;
+                }
+            };
+        }
+    }
+}

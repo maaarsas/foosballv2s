abdef81e06552c4c8f4afcbccbd6aad2dfddc6e2
DanielRitvas <uzastish@gmail.com>
2017-09-28 09:25:03 +0300

    add first test

---
+++ foosballv2s.Test/VideoFileTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using foosballv2s;
+using System.IO;
+
+namespace foosballv2s.Test
+{
+    [TestFixture]
+    public class VideoFileTests
+    {
+        [Test]
+        public void Cannot_Create_With_Bad_Path()
+        {
+            Assert.Throws<FileNotFoundException>(() => new VideoFile("/bad/path/to/file"));    
+        }
+    }
+}

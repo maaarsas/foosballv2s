82d8e1752a4222eece7f8b06a1049c6b24fd5e56
DanielRitvas <uzastish@gmail.com>
2017-09-28 10:01:18 +0300

    more tests

--- foosballv2s.Test/MovementDetectorTests.cs
+++ foosballv2s.Test/MovementDetectorTests.cs
@@ -1,33 +1,26 @@
 ﻿using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using foosballv2s;
-using System.IO;
 
 namespace foosballv2s.Test
 {
     [TestFixture]
-    public class VideoFileTests
+    public class MovementDetectorTests
     {
         String projectRootDirectory = Directory.GetParent(Directory.GetCurrentDirectory())
                     .Parent.FullName;
-
+        
         [Test]
-        public void Cannot_Create_With_Bad_Path()
+        public void DetectBall_Successful()
         {
-            Assert.Throws<FileNotFoundException>(() => new VideoFile("/bad/path/to/file"));    
-        }
-
-        //File not found
-        [Test]
-        public void Create_And_Check_Capture_Successful()
-        {
-            VideoFile videoFile = 
+            VideoFile videoFile =
                 new VideoFile(projectRootDirectory + "\\testData\\20170914_121625.mp4");
-            Assert.True(videoFile.capture.IsOpened);
+            MovementDetector movementDetector = new MovementDetector(videoFile);
+            Assert.True(movementDetector.DetectBall());
         }
     }
 }

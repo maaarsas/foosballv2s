54fb1ee5b029eeb940e79c18af7a48ba6eb58c06
Martynas Šapalas <martynas.sapalas@mif.stud.vu.lt>
2017-09-29 14:57:35 +0300

    Add missing Android app resources

---
+++ foosballv2s/VideoFile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Emgu.CV;
+using System.IO;
+
+namespace foosballv2s
+{
+    class VideoFile : IVideo
+    {
+        private VideoCapture capture;
+
+        public VideoFile(String file)
+        {
+            this.capture = new VideoCapture(file);
+            if (!capture.IsOpened)
+            {
+                throw new FileNotFoundException(file);
+            }
+        }
+
+        public Mat GetFrame()
+        {
+            return this.capture.QueryFrame();
+        }
+
+        public void Dispose()
+        {
+            this.capture.Stop();
+        }
+    }
+}

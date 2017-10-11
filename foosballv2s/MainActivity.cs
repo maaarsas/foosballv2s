8196589574e2ec9aca7c9fe1bd6236e38f51f896
IndirasM <I.Maziukas@gmail.com>
2017-10-11 18:58:21 +0300

    JSON included, working on search and load

--- foosballv2s/MainActivity.cs
+++ foosballv2s/MainActivity.cs
@@ -11,16 +11,48 @@ using Android.Provider;
 using Android.Runtime;
 using Emgu.CV.Structure;
 using Java.Interop;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
 
 namespace foosballv2s
 {
     [Activity()]
     public class MainActivity : Activity
     {
+        private EditText team1text, team2text;
+        private String team1name, team2name;
+        private String folder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal); //Possibly? Android.OS.Environment.DataDirectory.AbsolutePath;
+        private String file = "previousnames.json";
+        private List<String> names = new List<String>();
+        
         protected override void OnCreate(Bundle savedInstanceState)
         {
+
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.Main);
+
+            team1text = FindViewById<EditText>(Resource.Id.team1Name);
+            team2text = FindViewById<EditText>(Resource.Id.team2Name);
+            team1name = team1text.ToString();
+            team2name = team2text.ToString();
+
+            names.Add(team1name);
+            names.Add(team2name);
+
+            String path = folder + "/" + file;
+            String data = JsonConvert.SerializeObject(names);
+            File.WriteAllText(path, data);
+
+            var btnP = FindViewById<Button>(Resource.Id.prev);
+            btnP.Click += BtnPrev_Click;
+
+        }
+
+        private void BtnPrev_Click(object sender, EventArgs e)
+        {
+            Intent intent = new Intent(this, typeof(PreviousGamesActivity));
+            StartActivity(intent);
         }
 
         [Export("SubmitTeamNames")]
@@ -30,6 +62,8 @@ namespace foosballv2s
             Intent intent = new Intent(this, typeof(BallImageActivity));
             StartActivity(intent);
         }
+
+
 
     }
 }
---
+++ foosballv2s/PreviousGamesActivity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace foosballv2s
+{
+    [Activity(Label = "PreviousGamesActivity")]
+    public class PreviousGamesActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.PreviousGames);
+
+            string path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal)
+                + "/previousnames.json";
+
+            string jsonObj = File.ReadAllText("path");
+
+            List<String> names = new List<String>();
+            names = JsonConvert.DeserializeObject<List<String>>(jsonObj);
+        }
+    }
+}

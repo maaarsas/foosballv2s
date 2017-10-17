b0ac597e983c6bf790b99301fcfb23cb9aa8519c
IndirasM <I.Maziukas@gmail.com>
2017-10-17 22:55:40 +0300

    Code cleanup

--- foosballv2s/IO.cs
+++ foosballv2s/IO.cs
@@ -5,11 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Diagnostics;
+using Newtonsoft.Json;
+using Android.Widget;
 
 namespace foosballv2s
 {
     class IO
     {
+        private String path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal) + "/previousnames.json";
+        private Array dataRead;
+        private List<String> names = new List<String>();
+        private String team1name, team2name;
+        private String data;
+        SLSupp instance = new SLSupp();
+
         public void Write(string goalTime, string teamName, int totalScore, TimeSpan ts, Stopwatch timer)
         {
             string filePath = @"C:\Users\Radvila\Documents\GitHub\foosballv2s\foosballv2s\GameInfo.txt";
@@ -23,7 +32,9 @@ namespace foosballv2s
             }
         }
 
-        public void WriteTeamNames(Team team1, Team team2)
+        //Since we've decided on using JSON for saving/reading these are not required. May be used in later functionality, such as statistics
+
+        /*public void WriteTeamNames(Team team1, Team team2)
         {
             string filePath = @"C:\Users\Radvila\Documents\GitHub\foosballv2s\foosballv2s\GameInfo.txt";
 
@@ -40,6 +51,37 @@ namespace foosballv2s
 
             team1.teamName = File.ReadLines(filePath).First();
             team2.teamName = File.ReadLines(filePath).Skip(2).Take(1).First();
+        }*/
+
+        public Array Read_Deserialize() {
+            if (!System.IO.File.Exists(path))
+            {
+                System.IO.FileStream fs = System.IO.File.Create(path);
+                fs.Dispose();
+            }
+            else
+            {
+                var data = File.ReadAllText(path);
+                names = JsonConvert.DeserializeObject<List<String>>(data);
+
+                //Need help with this IF clause (in SLSupp class). Need to recreate list if file exists and is empty. Solutions?
+
+                names = instance.CheckPreload(names);
+            }
+            dataRead = names.ToArray();
+            return dataRead;
+        }
+
+        public void Write_Serialize(AutoCompleteTextView team1text, AutoCompleteTextView team2text) {
+
+            team1name = team1text.Text;
+            team2name = team2text.Text;
+
+            instance.CheckPreload(names);
+            names = instance.PreSaveCheck(team1name, team2name, names);
+
+            data = JsonConvert.SerializeObject(names);
+            File.WriteAllText(path, data);
         }
     }
 }
--- foosballv2s/MainActivity.cs
+++ foosballv2s/MainActivity.cs
@@ -20,61 +20,34 @@ namespace foosballv2s
     [Activity()]
     public class MainActivity : Activity
     {
-        private AutoCompleteTextView t1, t2;
-        private EditText team1text, team2text;
-        private String team1name, team2name;
-        private String path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal) + "/previousnames.json";
-        //private String file = "previousnames.json";
-        private List<String> names = new List<String>();
-        private String data;
-        
+        private AutoCompleteTextView t1, t2, team1text, team2text;
+        IO instance = new IO();
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
 
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.Main);
 
-            if (!System.IO.File.Exists(path))
-            {
-                System.IO.FileStream fs = System.IO.File.Create(path);
-                fs.Dispose();
-            }
-            else
-            {
-                data = File.ReadAllText(path);
-                names = JsonConvert.DeserializeObject<List<String>>(data);
-
-                //Need help with this IF clause. Need to recreate list if file exists and is empty. Solutions?
-
-                if (names == null)
-                {
-                    names = new List<String>();
-                    names.Add("Team 1");
-                    names.Add("Team 2");
-                }
-            }
-
             t1 = (AutoCompleteTextView)FindViewById<AutoCompleteTextView>(Resource.Id.team1Name);
             t2 = (AutoCompleteTextView)FindViewById<AutoCompleteTextView>(Resource.Id.team2Name);
 
-            var dataRead = names.ToArray();
-
-            ArrayAdapter adapter = new ArrayAdapter(this, Resource.Layout.support_simple_spinner_dropdown_item, dataRead);
+            ArrayAdapter adapter = new ArrayAdapter(this, Resource.Layout.support_simple_spinner_dropdown_item, instance.Read_Deserialize());
 
             t1.Adapter = adapter;
             t2.Adapter = adapter;
 
-            var btnP = FindViewById<Button>(Resource.Id.prev);
-            btnP.Click += BtnPrev_Click;
+            /*var btnP = FindViewById<Button>(Resource.Id.prev);
+            btnP.Click += BtnPrev_Click;*/
 
         }
 
-        private void BtnPrev_Click(object sender, EventArgs e)
+        /*private void BtnPrev_Click(object sender, EventArgs e)
         {
             Intent intent = new Intent(this, typeof(PreviousGamesActivity));
 
             StartActivity(intent);
-        }
+        }*/
 
         [Export("SubmitTeamNames")]
         public void SubmitTeamNames(View view)
@@ -85,42 +58,10 @@ namespace foosballv2s
 
             team1text = (AutoCompleteTextView)FindViewById<AutoCompleteTextView>(Resource.Id.team1Name);
             team2text = (AutoCompleteTextView)FindViewById<AutoCompleteTextView>(Resource.Id.team2Name);
-            team1name = team1text.Text;
-            team2name = team2text.Text;
 
-            int flag1 = 0;
-            int flag2 = 0;
-            foreach (var name in names)
-            {
-                if(team1name == name)
-                {
-                    flag1 = 1;
-                }
-                if(team2name == name)
-                {
-                    flag2 = 1;
-                }
-            }
-
-            if (flag1 == 0)
-            {
-                names.Add(team1name);
-            }
-            if (flag2 == 0)
-            {
-                names.Add(team2name);
-            }
-            
-
-            data = JsonConvert.SerializeObject(names);
-            File.WriteAllText(path, data);
-
-            Console.WriteLine(data);
+            instance.Write_Serialize(team1text, team2text);
 
             StartActivity(intent);
         }
-
-
-
     }
 }

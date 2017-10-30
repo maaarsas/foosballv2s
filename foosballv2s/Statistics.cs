c0013f0010ba5b386907b5d0eca01f022a18bc1c
IndirasM <I.Maziukas@gmail.com>
2017-10-30 12:20:08 +0200

    Branch update

---
+++ foosballv2s/Statistics.cs
@@ -0,0 +1,16 @@
+﻿
+using Android.App;
+using Android.OS;
+
+namespace foosballv2s
+{
+    [Activity(Label = "Statistics")]
+    public class Statistics : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+        }
+    }
+}

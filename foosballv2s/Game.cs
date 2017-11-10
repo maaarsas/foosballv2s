0e858d63a001e84a92358f5275dcd34f02a54462
Martynas Šapalas <martynas.sapalas@gmail.com>
2017-11-10 12:53:11 +0200

    Add a web service project

--- foosballv2s/Game.cs
+++ foosballv2s/Game.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
+using System.ComponentModel.DataAnnotations.Schema;
 using Emgu.CV.Fuzzy;
 using Emgu.CV.Structure;
 using Javax.Security.Auth;
@@ -12,18 +14,33 @@ using Xamarin.Forms;
 [assembly: Dependency(typeof(foosballv2s.Game))]
 namespace foosballv2s
 {
+    [Table("games")]
     public class Game
     {
         public const int MAX_SCORE = 7;
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int id;
         
         private int team1Score = 0;
         private int team2Score = 0;
 
+        [NotMapped]
         public Hsv BallColor { get; set; }
 
+        [ForeignKey("Team1")] 
+        public int Team1Id { get; set; }
+        
         public Team Team1 { get; set; } = new Team();
+        
+        [ForeignKey("Team2")] 
+        public int Team2Id { get; set; }
+        
         public Team Team2 { get; set; } = new Team();
         
+        [Column("team1_score")]
         public int Team1Score
         {
             get { return team1Score; }
@@ -38,6 +55,7 @@ namespace foosballv2s
             }
         }
 
+        [Column("team2_score")]
         public int Team2Score
         {
             get { return team2Score; }
@@ -52,6 +70,7 @@ namespace foosballv2s
             }
         }
 
+        [NotMapped]
         public Boolean HasEnded { get; private set; } = false;
 
         private void CheckGameEnd()
--- foosballv2s/Team.cs
+++ foosballv2s/Team.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace foosballv2s
 {
+    [Table("teams")]
     public class Team
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int id;
+        
+        [Column("name")]
         public string TeamName { get; set; }
+        
+        [NotMapped]
         public int TotalScore { get; set; }
     }
 }

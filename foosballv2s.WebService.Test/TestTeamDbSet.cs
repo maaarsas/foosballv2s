c9f12f4dbea0b654733fc7dee9ab8d94a1b8d33d
Martynas Šapalas <martynas.sapalas@gmail.com>
2017-11-11 11:49:59 +0200

    Add database sets for testing

---
+++ foosballv2s.WebService.Test/TestTeamDbSet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using foosballv2s.WebService.Models;
+
+namespace foosballv2s.WebService.Test
+{
+    class TestTeamDbSet : TestDbSet<Team>
+    {
+        public override Team Find(params object[] keyValues)
+        {
+            return this.SingleOrDefault(product => product.Id == (int)keyValues.Single());
+        }
+    }
+}

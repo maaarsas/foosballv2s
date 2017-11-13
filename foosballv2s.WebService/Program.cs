5cda14f6191cb607c2b2efb34c4c8d48e0c12545
Martynas Šapalas <martynas.sapalas@gmail.com>
2017-11-13 17:08:01 +0200

    Change web service ip adress to allow not only local connections

--- foosballv2s.WebService/Program.cs
+++ foosballv2s.WebService/Program.cs
@@ -19,6 +19,7 @@ namespace foosballv2s.WebService
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://0.0.0.0:5000")
                 .UseStartup<Startup>()
                 .Build();
     }

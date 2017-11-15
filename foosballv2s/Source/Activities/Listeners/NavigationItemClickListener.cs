4f583f23fafdd033f1e6de1ad1168620db320c8c
Martynas Šapalas <martynas.sapalas@gmail.com>
2017-11-15 11:55:17 +0200

    Implement navigation items selection events listener

---
+++ foosballv2s/Source/Activities/Listeners/NavigationItemClickListener.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Support.Design.Internal;
+using Android.Support.Design.Widget;
+using Android.Views;
+using Android.Widget;
+
+namespace foosballv2s.Listeners
+{
+    public class NavigationItemClickListener : Java.Lang.Object, NavigationView.IOnNavigationItemSelectedListener
+    {
+        private Activity currentActivity;
+        private NavigationMenuView menu;
+
+        public NavigationItemClickListener(Activity activity)
+        {
+            currentActivity = activity;
+        }
+        
+        public bool OnNavigationItemSelected(IMenuItem menuItem)
+        {
+            System.Type intentType;
+            switch (menuItem.ItemId)
+            {
+                case Resource.Id.nav_teams:
+                {
+                    intentType = typeof(TeamsActivity);
+                    break;
+                }
+                case Resource.Id.nav_games:
+                {
+                    intentType = typeof(GamesActivity);
+                    break;
+                }
+                case Resource.Id.nav_tournaments:
+                {
+                    intentType = typeof(TournamentsActivity);
+                    break;
+                }
+                default:
+                {
+                    intentType = typeof(MainActivity);
+                    break;
+                }
+            }
+            Intent intent = new Intent(Application.Context, intentType);
+            currentActivity.StartActivity(intent);
+            currentActivity.Finish();
+        }
+    }
+}

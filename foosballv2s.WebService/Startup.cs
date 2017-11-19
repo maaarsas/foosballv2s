b820fd3d063a2b90938dad0860d64ca0355882fa
Martynas Šapalas <martynas.sapalas@gmail.com>
2017-11-19 15:21:19 +0200

    Add other needed packages for authentication

--- foosballv2s.WebService/Startup.cs
+++ foosballv2s.WebService/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Owin;
 
 namespace foosballv2s.WebService
 {

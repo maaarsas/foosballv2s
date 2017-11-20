56cbe62632baad39c9e990109190715daccb252f
Martynas Šapalas <martynas.sapalas@gmail.com>
2017-11-20 18:14:33 +0200

    Add authentication repository

--- foosballv2s.WebService/Models/IdentityContext.cs
+++ foosballv2s.WebService/Models/IdentityContext.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace foosballv2s.WebService.Models
 {
-    public class IdentityContext : IdentityDbContext<User>
+    public class IdentityContext : IdentityDbContext<User>, IIdentityContext
     {
         public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
         {

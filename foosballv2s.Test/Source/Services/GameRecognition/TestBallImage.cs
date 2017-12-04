b7695a26ec8d8004e519697d9a771aa5307a4bc3
Martynas Šapalas <martynas.sapalas@gmail.com>
2017-12-04 22:51:55 +0200

    Extend unit tsts for color recognition

--- foosballv2s.Test/Source/Services/GameRecognition/TestBallImage.cs
+++ foosballv2s.Test/Source/Services/GameRecognition/TestBallImage.cs
@@ -1,4 +1,7 @@
-﻿using Android.Graphics;
+﻿using Android.App;
+using Android.Content;
+using Android.Content.Res;
+using Android.Graphics;
 using Emgu.CV.Structure;
 using foosballv2s.Droid.Shared.Source.Services.GameRecognition;
 using NUnit.Framework;
@@ -8,14 +11,13 @@ namespace foosballv2s.Test.Source.Services.GameRecognition
     [TestFixture]
     public class TestBallImage
     {
-        [Test]
-        public void TestGetColor()
+        [Test, TestCaseSource("Bitmaps")]
+        public void TestGetColor(Bitmap bitmap, Color ballColor)
         {
-            Hsv imageColor = new Hsv(Color.Green.GetHue() / 2, Color.Green.GetSaturation() * 255,
-                Color.Green.GetBrightness() * 255);
-            Bitmap bitmap = Bitmap.CreateBitmap(200, 200, Bitmap.Config.Argb8888);
-            bitmap.EraseColor(Color.Green);
-
+            Hsv imageColor = new Hsv(ballColor.GetHue() / 2,   
+                ballColor.GetSaturation() * 255,  
+                ballColor.GetBrightness() * 255);
+            
             BallImage ballImage = new BallImage(bitmap);
             Hsv detectedColor = ballImage.getColor();
 
@@ -23,5 +25,33 @@ namespace foosballv2s.Test.Source.Services.GameRecognition
             Assert.AreEqual(imageColor.Satuation, detectedColor.Satuation);
             Assert.AreEqual(imageColor.Value, detectedColor.Value);
         }
+
+        private static object[] Bitmaps =
+        {
+            new object[]
+            {
+                GetBitmapWithColor(Color.Green, 200, 200),
+                Color.Green,
+            },
+            new object[]
+            {
+                BitmapFactory.DecodeResource(Application.Context.Resources, Resource.Drawable.ball_image_1),
+                new Color(3, 169, 244),
+            },
+            new object[]
+            {
+                BitmapFactory.DecodeResource(Application.Context.Resources, Resource.Drawable.ball_image_2),
+                new Color(255, 235, 59),
+            },
+        };
+
+        private static Bitmap GetBitmapWithColor(Color color, int width, int height)
+        {
+            var bitmap = Bitmap.CreateBitmap(width, height, Bitmap.Config.Argb8888);
+            bitmap.EraseColor(Color.Green);
+            return bitmap;
+        }
+
+
     }
 }

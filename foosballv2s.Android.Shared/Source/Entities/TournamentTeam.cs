9f1f49b25f3a026049f9ebcb26e947d16bcbc356
DanielRitvas <uzastish@gmail.com>
2017-12-19 23:14:53 +0200

    Create other models

--- foosballv2s.Android.Shared/Source/Entities/TournamentTeam.cs
+++ foosballv2s.Android.Shared/Source/Entities/TournamentTeam.cs
@@ -8,20 +8,13 @@ using foosballv2s.Droid.Shared.Source.Services.GameLogger;
 using Newtonsoft.Json;
 using Xamarin.Forms;
 
-[assembly: Dependency(typeof(TournamentPair))]
+[assembly: Dependency(typeof(TournamentTeam))]
 namespace foosballv2s.Droid.Shared.Source.Entities
 {
-    public class TournamentPair
+    public class TournamentTeam
     {
         public int Id { get; set; }
 
-        public int GamePairNumberInStage { get; set; }
-
-        public int StageNumber { get; set; } = 1;
-
-        public Team Team1 { get; set; }
-        public Team Team2 { get; set; }
-
-        public Game Game { get; set; }
+        public Team Team { get; set; }
     }
-} 
+}
